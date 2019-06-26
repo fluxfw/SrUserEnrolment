089c81d84dd655ef3976037abf702b8705a4ab31
Florian Wyss <fw@studer-raimann.ch>
2019-06-26 11:18:15 +0200

    Reset password link same size

--- classes/class.ilSrUserEnrolmentUIHookGUI.php
+++ classes/class.ilSrUserEnrolmentUIHookGUI.php
@@ -62,15 +62,18 @@ class ilSrUserEnrolmentUIHookGUI extends ilUIHookPluginGUI {
 
 								self::dic()->ctrl()->setParameterByClass(ResetPasswordGUI::class, Repository::GET_PARAM_USER_ID, $user_id);
 
+								$reset_password_link = self::output()->getHTML(self::dic()->ui()->factory()->link()->standard(self::plugin()
+									->translate("button", ResetPasswordGUI::LANG_MODULE_RESET_PASSWORD), self::dic()->ctrl()->getLinkTargetByClass([
+										ilUIPluginRouterGUI::class,
+										ResetPasswordGUI::class
+									], ResetPasswordGUI::CMD_RESET_PASSWORD_CONFIRM)));
+
+								$reset_password_link = str_replace('<a ', '<a class="il_ContainerItemCommand2" ', $reset_password_link);
+
 								return self::output()->getHTML([
 									$link,
 									"<br>",
-									self::dic()->ui()->factory()->link()->standard(self::plugin()
-										->translate("button", ResetPasswordGUI::LANG_MODULE_RESET_PASSWORD), self::dic()->ctrl()
-										->getLinkTargetByClass([
-											ilUIPluginRouterGUI::class,
-											ResetPasswordGUI::class
-										], ResetPasswordGUI::CMD_RESET_PASSWORD_CONFIRM))
+									$reset_password_link
 								]);
 							} else {
 								return $link;
