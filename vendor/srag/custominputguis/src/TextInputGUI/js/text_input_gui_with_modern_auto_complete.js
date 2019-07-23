95f1c9c4a2840f126f915461621912b91c925bf3
Florian Wyss <fw@studer-raimann.ch>
2019-07-23 11:30:07 +0200

    excel import field autocomplete

---
+++ vendor/srag/custominputguis/src/TextInputGUI/js/text_input_gui_with_modern_auto_complete.js
@@ -0,0 +1,92 @@
+(() => {
+	if (il.int_textinput_observer) {
+		return;
+	}
+	il.int_textinput_observer = true;
+
+	const observer = new MutationObserver((events) => {
+		/**
+		 * @type {MutationRecord} event
+		 */
+		for (const event of events) {
+			//console.log(event);
+			for (const node of event.addedNodes) {
+				if (node instanceof Element) { // Some node types like text missing some methods, this can be ignored
+					for (const child_node of [node, ...node.querySelectorAll("*")]) { // Check item self and needs also to check all children because if only insert a new parent node with HTML, the needed node is missing in addedNodes
+						if (checkNode(child_node)) {
+							initNode(child_node);
+						}
+					}
+				}
+			}
+		}
+	});
+
+	observer.observe(document.documentElement, {
+		childList: true,
+		subtree: true
+	});
+
+	/**
+	 * @param {Node} node
+	 *
+	 * @returns {boolean}
+	 */
+	function checkNode(node) {
+		return (node instanceof HTMLInputElement && node.type === "text" && node.classList.contains("form-control"));
+	}
+
+	/**
+	 * @param {HTMLInputElement} node
+	 */
+	function initNode(node) {
+		if (node._init_textinput) {
+			return;
+		}
+		node._init_textinput = true;
+
+		//console.log(node);
+
+		if (node.dataset.autocomplete_url) {
+			$.widget("custom.iladvancedautocomplete", $.ui.autocomplete, {
+				more: false,
+				_renderMenu: function (ul, items) {
+					for (const item of items) {
+						this._renderItemData(ul, item);
+					}
+
+					this.options.requestUrl.searchParams.delete("fetchall");
+
+					if (this.more) {
+						ul.append(`<li class='ui-menu-category ui-menu-more ui-state-disabled'><span>&raquo;${il.textinput_more_txt}</span></li>`);
+						ul.find('li').last().on('click', function (e) {
+							this.options.requestUrl.searchParams.append("fetchall", "1");
+							this.close(e);
+							this.search(null, e);
+							e.preventDefault();
+						}.bind(this));
+					}
+				}
+			});
+
+			$(node).iladvancedautocomplete({
+				requestUrl: new URL(node.dataset.autocomplete_url, location.href),
+				source: async function (request, response) {
+					const url = new URL(this.options.requestUrl);
+					url.searchParams.append("term", request.term);
+
+					const data = await (await fetch(url)).json();
+
+					if (typeof data.items === "undefined") {
+						this.more = false;
+						response(data);
+					} else {
+						this.more = data.hasMoreResults;
+						response(data.items);
+					}
+				},
+				minLength: 0
+			});
+		}
+	}
+})();
