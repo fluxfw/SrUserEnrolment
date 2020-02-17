305c32d6fb9e99c4d4ff93a3b34abca504b89a94
Florian Wyss <fw@studer-raimann.ch>
2020-02-17 13:29:34 +0100

    Fix

--- classes/class.ilSrUserEnrolmentUIHookGUI.php
+++ classes/class.ilSrUserEnrolmentUIHookGUI.php
@@ -170,9 +170,9 @@ class ilSrUserEnrolmentUIHookGUI extends ilUIHookPluginGUI
             self::dic()->ctrl()->setParameterByClass(RequestInfoGUI::class, RequestInfoGUI::GET_PARAM_REQUEST_ID, $request_id);
 
             if ($request !== null && $request->getUserId() === intval(self::dic()->user()->getId())) {
-                self::dic()->ctrl()->redirectByClass([ilUIPluginRouterGUI::class, RequestsGUI::class, RequestInfoGUI::class], RequestInfoGUI::CMD_SHOW_WORKFLOW);
-            } else {
                 self::dic()->ctrl()->redirectByClass([ilUIPluginRouterGUI::class, RequestInfoGUI::class], RequestInfoGUI::CMD_SHOW_WORKFLOW);
+            } else {
+                self::dic()->ctrl()->redirectByClass([ilUIPluginRouterGUI::class, RequestsGUI::class, RequestInfoGUI::class], RequestInfoGUI::CMD_SHOW_WORKFLOW);
             }
         }
     }
