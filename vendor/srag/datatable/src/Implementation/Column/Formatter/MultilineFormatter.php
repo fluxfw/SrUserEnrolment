430471f884c3cdb152f9b510379bb177ae8c6c3d
sragfw <fw@studer-raimann.ch>
2021-01-29 12:46:22 +0100

    Update composer

---
+++ vendor/srag/datatable/src/Implementation/Column/Formatter/MultilineFormatter.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace srag\DataTableUI\SrUserEnrolment\Implementation\Column\Formatter;
+
+use srag\DataTableUI\SrUserEnrolment\Component\Column\Column;
+use srag\DataTableUI\SrUserEnrolment\Component\Data\Row\RowData;
+use srag\DataTableUI\SrUserEnrolment\Component\Format\Format;
+
+/**
+ * Class MultilineFormatter
+ *
+ * @package srag\DataTableUI\SrUserEnrolment\Implementation\Column\Formatter
+ *
+ * @author  studer + raimann ag - Team Custom 1 <support-custom1@studer-raimann.ch>
+ */
+class MultilineFormatter extends DefaultFormatter
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function formatRowCell(Format $format, $value, Column $column, RowData $row, string $table_id) : string
+    {
+        return nl2br(implode("\n", array_map("htmlspecialchars", explode("\n", strval($value)))), false);
+    }
+}
